644ae8eea7802893314436c442981c07ed0cee05
mandrysz <01187204@pw.edu.pl>
2025-11-16 14:01:13 +0100

    Created test pages

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,22 @@
+'use client';
+
 import { Button } from '@/components/Button';
+import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Home() {
-  return <Button className="m-4">Hello</Button>;
+  const [count, setCount] = useState<number>(0);
+
+  const handleClick = () => {
+    setCount((count) => count + 1);
+  };
+
+  return (
+    <div>
+      <Button onClick={handleClick} className="m-4">
+        Clicked {count} times
+      </Button>
+      <Link href="/test">Go to test page</Link>
+    </div>
+  );
 }
---
+++ client/src/app/test/page.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function TestPage() {
+  return (
+    <div className="m-2">
+      <Link href="/">Go back to home page</Link>
+    </div>
+  );
+}
