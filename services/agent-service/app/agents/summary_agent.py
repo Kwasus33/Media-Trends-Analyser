fb86f0fd2de27ae76d2f36ecc359cb43e18890e2
Kacper Siemionek <siemionek.kacper22@gmail.com>
2025-11-17 15:35:21 +0100

    add generate_periodic_summary function

--- services/agent-service/app/agents/summary_agent.py
+++ services/agent-service/app/agents/summary_agent.py
@@ -5,7 +5,7 @@ from datetime import date
 from app.agents.agent_config import AgentSettings
 from app.schemas.article import Article
 from app.schemas.daily_summary import DailySummary
-from app.schemas.period_summary import PeriodSummary
+from app.schemas.periodic_summary import PeriodicSummary
 
 
 class SummaryAgent:
@@ -37,4 +37,22 @@ class SummaryAgent:
             summary=json_summary["summaries"],
             categories=json_summary["categories"],
             references=json_summary["references"]
+        )
+
+    def generate_periodic_summary(self, daily_summaries: list[DailySummary], start_date: date, end_date: date):
+        prompt = self.settings.periodic_summary_prompt.format(daily_summaries=daily_summaries)
+        config = GenerateContentConfig(response_mime_type="application/json")
+
+        json_summary = json.loads(self.generate_content(prompt, config))
+
+        return PeriodicSummary(
+            start_date=start_date,
+            end_date=end_date,
+            main_summary=json_summary["main_summary"],
+            categories_timeline=json_summary["categories_timeline"],
+            category_totals=json_summary["category_totals"],
+            trends=json_summary["trends"],
+            key_insights=json_summary["key_insights"],
+            source_highlights=json_summary["source_highlights"],
+            event_timeline=json_summary["event_timeline"]
         )
