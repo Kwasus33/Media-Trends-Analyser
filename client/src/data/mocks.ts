76d17e72519ffb55b3cf8a9111a62227bc19e647
Pedziwiatrr <michalozaurus@gmail.com>
2025-11-18 21:24:31 +0100

    test(ui): add category trend mock data to test new charts.

---
+++ client/src/data/mocks.ts
@@ -0,0 +1,7 @@
+export const mockCategoryData = [
+  { name: 'Technology', value: 67 },
+  { name: 'Politics', value: 40 },
+  { name: 'Economy', value: 15 },
+  { name: 'Sport', value: 24 },
+  { name: 'Culture', value: 53 },
+];
