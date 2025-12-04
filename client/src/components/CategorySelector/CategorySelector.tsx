7a45bb647ce394ff44ec62c680ae1c4696d79293
Pedziwiatrr <michalozaurus@gmail.com>
2025-12-04 12:47:03 +0100

    feat: changed category buttons style to match sources buttons

--- client/src/components/CategorySelector/CategorySelector.tsx
+++ client/src/components/CategorySelector/CategorySelector.tsx
@@ -13,7 +13,13 @@ export const CategorySelector = ({
 }: CategorySelectorProps) => {
   return (
     <label
-      className={`cursor-pointer px-4 py-1.5 text-sm font-medium rounded-full border transition-colors select-none ${checked ? 'bg-white text-black border-white' : 'bg-transparent text-gray-400 border-gray-600 hover:border-gray-400'}`}
+      className={`cursor-pointer px-4 py-1.5 text-sm border rounded-xl transition-all duration-300 select-none
+        ${
+          checked
+            ? 'font-bold border-2 text-white border-white bg-gray-800'
+            : 'font-medium text-gray-400 border-gray-700 hover:border-gray-500 hover:text-gray-300'
+        }
+      `}
     >
       <input
         type="checkbox"
