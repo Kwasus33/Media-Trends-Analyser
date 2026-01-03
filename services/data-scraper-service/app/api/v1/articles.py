f63147038890aac84e5174fc5c740338cf26fe54
Jakub Kwaśniak <kubak04@outlook.com>
2026-01-03 19:45:03 +0100

    feat: adds endpoint for scraping all sources + creates exception handlers for app

--- services/data-scraper-service/app/api/v1/articles.py
+++ services/data-scraper-service/app/api/v1/articles.py
@@ -1,19 +1,26 @@
-from app.scrapers.base_scraper import API_SCRAPERS
-from app.core.config import CONTEXT
+from app.services.article_service import ScraperService
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Query
 
-# import os
-
-
-router = APIRouter(
-    prefix="/articles", tags="articles", responses={404: {"description": "Not found"}}
-)
+router = APIRouter(prefix="/articles", responses={404: {"description": "Not found"}})
+scraper_service = ScraperService()
 
 # need to think of adding start/end datetime for scrapping api with timestamp,
 # can be added as separate enpoint if api would be separated to rss, api enpoints or start/end can be query params
 
 # those enpoints can be used by client for generating live (tho calculations-demanding) results
+
+
+@router.get("/all")
+async def get_all_articles(limit: int | None = Query(default=None, ge=1)):
+    """
+    Gets all available articles from online sources like APIs and RSS feeds
+    """
+    return (
+        scraper_service.fetch_all_articles(limit)
+        if limit
+        else scraper_service.fetch_all_articles()
+    )
 
 
 @router.get("/{source}")
@@ -27,24 +34,4 @@ async def get_articles(source: str, category: str | None = None):
     :param category: catrgory of source entries to get, None if source categories are not defined
     :type category: str | None
     """
-    try:
-        source_data = CONTEXT[source]
-        base_url = source_data["endpoint"]["base_url"]
-        scraper = API_SCRAPERS[source_data["type"]]
-
-    except HTTPException(
-        status_code=404, detail="Item not found - wrong data passed"
-    ) as e:
-        print(
-            f"Invalid query path - source or category does not match available ones: {e}"
-        )
-
-    if category and category not in source_data["endpoint"]["categories"]:
-        raise HTTPException("Item not found - wrong category passed")
-
-    if not category and source_data["endpoint"]["categories"]:
-        category = source_data["endpoint"]["categories"][0]
-
-    scraper = scraper(base_url)
-
-    return scraper.collect_data(category)
+    return scraper_service.fetch_articles(source, category)
