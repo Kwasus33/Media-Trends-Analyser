fed892cda72639a929603823d851cb544d205273
Jakub Kwaśniak <kubak04@outlook.com>
2026-01-03 14:28:25 +0100

    feat: adds article entry orm model

---
+++ services/data-scraper-service/app/models/articles.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Integer, String, Text, DateTime, ARRAY, Identity
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from datetime import datetime
+
+
+class Base(DeclarativeBase): ...
+
+
+class AticleDB(Base):
+    __tablename__ == "articles"
+
+    id: Mapped[int] = mapped_column(Integer, Identity(), primary_key=True)
+    title: Mapped[str | None] = mapped_column(String, nullable=True)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    url: Mapped[str] = mapped_column(String, nullable=False)
+    published_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False
+    )
+    source: Mapped[str | None] = mapped_column(String, nullable=True)
+    category: Mapped[list[str]] = mapped_column(ARRAY(String), nullable=False)
